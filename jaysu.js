358e1f9f755f31f131baa68946364371ca01a08d
jayasooryakj <79013733+jayasooryakj@users.noreply.github.com>
2021-08-18 09:31:57 +0530

    Add files via upload

---
+++ jaysu.js
@@ -0,0 +1,19 @@
+function makeSilver()
+{
+    document.body.style.backgroundColor="silver";
+}
+
+function makeViolet()
+{
+    document.body.style.backgroundColor="violet";
+}
+
+function makeWood()
+{
+    document.body.style.backgroundColor="burlywood";
+}
+
+function makeBlue()
+{
+    document.body.style.backgroundColor="cadetblue";
+}
